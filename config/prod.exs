3035e1be94df4da7f63b48e3b0e8ec804ca55e93
Sonam Choejur Tshering <sonamtshering16@gmail.com>
2021-05-24 15:36:27 +0600

    removed prod.secret since we dont need in heroku

--- config/prod.exs
+++ config/prod.exs
@@ -16,6 +16,13 @@ config :reborn, RebornWeb.Endpoint,
   url: [scheme: "https", host: "reborn-alphanapster.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+config :reborn, Reborn.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -56,4 +63,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which loads secrets
 # and configuration from environment variables.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
